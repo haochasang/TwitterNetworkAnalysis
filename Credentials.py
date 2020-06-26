ec49787703005d6d4187353cfefad6ead8d190c3
ChelseaHe <chenhe@brandeis.edu>
2020-06-25 23:24:43 -0400

    Add sample Credentials.py

---
+++ Credentials.py
@@ -0,0 +1,5 @@
+# Set up Twitter API credentials
+consumer_key = 'YOUR_CONSUMER_KEY'
+consumer_secret = 'YOUR_CONSUMER_SECRET'
+access_token = 'YOUR_ACCESS_TOKEN'
+access_secret = 'YOUR_ACCESS_TOKEN_SECRET'
